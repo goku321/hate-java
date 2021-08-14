2bf001496eb5fa02fdd9e703a0acdff91668f29b
Deepak <deepak.sah@nutanix.com>
2021-08-15 00:05:13 +0530

    Change commit email

---
+++ builder-pattern/NutritionFacts.java
@@ -0,0 +1,64 @@
+public class NutritionFacts {
+    private final int servingSize;
+    private final int servings;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbs;
+
+    public static class Builder {
+        // Required parameters
+        private final int servingSize;
+        private final int servings;
+
+        // Optional parameters
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbs = 0;
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder calories(int val) {
+            calories = val;
+            return this;
+        }
+
+        public Builder fat(int val) {
+            fat = val;
+            return this;
+        }
+
+        public Builder sodium(int val) {
+            sodium = val;
+            return this;
+        }
+
+        public Builder carbs(int val) {
+            carbs = val;
+            return this;
+        }
+
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        servingSize = builder.servingSize;
+        servings = builder.servings;
+        calories = builder.calories;
+        fat = builder.fat;
+        sodium = builder.sodium;
+        carbs = builder.carbs;
+    }
+
+    public static void main(String[] args) {
+        NutritionFacts whey = new NutritionFacts.Builder(30, 1)
+            .calories(177).fat(1).carbs(20).build();
+        System.out.println(whey.servingSize);
+    }
+}
